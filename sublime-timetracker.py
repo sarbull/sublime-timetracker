a7ff07d09e52f43d424ae522bda89ef7b6423dd3
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-01-30 12:15:02 +0200

    updated home and output file

--- sublime-timetracker.py
+++ sublime-timetracker.py
@@ -6,15 +6,15 @@ class TimetrackerCommand(sublime_plugin.EventListener):
   def on_post_save(self, view):
     # string = open("/Users/sarbull/.sublime-timetracker", 'r+').read()
 
-    save_to = expanduser("~/.sublime-timetracker")
+    home = expanduser("~")
 
-    string = open(save_to, 'r+').read()
+    string = open(home + '/.sublime-timetracker', 'r+').read()
 
     # Get the filename of your saved file
     saved_file = view.file_name()
 
     # Check if your in any of your development projects   
-    #if 'dev/projets' in saved_file:
+    #if 'dev/projes' in saved_file:
     #project = saved_file.split('projects/')[1].split('/')[0]
     #output = project + ":" + str(time.time()) + "\n"
     output = saved_file + ":" + str(time.time()) + "\n"
